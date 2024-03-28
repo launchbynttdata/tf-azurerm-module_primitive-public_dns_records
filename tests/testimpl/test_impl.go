7a731ed9da63b71ada8d2fa184b58fd659176a11
Ben Vaughan <ben.vaughan@nttdata.com>
2024-03-28 11:34:01 -0500

    'Migration validation fixes for make check'

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -2,14 +2,18 @@ package common
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/profiles/latest/dns/mgmt/dns"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/dns/armdns"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/launchbynttdata/lcaf-component-terratest/lib/azure/configure"
-	internalDns "github.com/launchbynttdata/lcaf-component-terratest/lib/azure/dns"
-	"github.com/launchbynttdata/lcaf-component-terratest/lib/azure/login"
 	"github.com/launchbynttdata/lcaf-component-terratest/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -17,19 +21,28 @@ import (
 const terraformDir string = "../../examples/dns_zone_records"
 const varFile string = "test.tfvars"
 
-func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
-	envVarMap := login.GetEnvironmentVariables()
-	clientID := envVarMap["clientID"]
-	clientSecret := envVarMap["clientSecret"]
-	tenantID := envVarMap["tenantID"]
-	subscriptionID := envVarMap["subscriptionID"]
+// TODO: Add support for other cloud configurations
+const azEnvironment string = "AZUREPUBLICCLOUD"
 
-	spt, err := login.GetServicePrincipalToken(clientID, clientSecret, tenantID)
+func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
+	subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	if subscriptionID == "" {
+		t.Fatalf("ARM_SUBSCRIPTION_ID must be set for acceptance tests")
+	}
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
-		t.Fatalf("Error getting Service Principal Token: %v", err)
+		t.Fatalf("Error getting credentials: %e\n", err)
+	}
+	cloudCfg, err := getCloudConfiguration(azEnvironment)
+	if err != nil {
+		t.Fatalf("Error interpreting Azure Cloud type: %e\n", err)
 	}
 
-	dnsZoneRecordSetsClient := internalDns.GetDNSZoneRecordSetsClient(spt, subscriptionID)
+	clientOpt := policy.ClientOptions{Cloud: cloudCfg}
+	dnsZoneRecordSetsClient, err := armdns.NewRecordSetsClient(subscriptionID, credential, &arm.ClientOptions{ClientOptions: clientOpt})
+	if err != nil {
+		t.Fatalf("Error creating DNS Zone Record Sets Client: %e\n", err)
+	}
 
 	terraformOptions := configure.ConfigureTerraform(terraformDir, []string{terraformDir + "/" + varFile})
 
@@ -38,24 +51,25 @@ func TestDnsZoneRecords(t *testing.T, ctx types.TestContext) {
 	})
 }
 
-func checkDNSZoneRecordSets(t *testing.T, dnsZoneRecordSetsClient dns.RecordSetsClient, terraformOptions *terraform.Options) {
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "a_record_ids", "a_record_fqdns", dns.A)
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "cname_record_ids", "cname_record_fqdns", dns.CNAME)
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "ns_record_ids", "ns_record_fqdns", dns.NS)
-	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "txt_record_ids", "txt_record_fqdns", dns.TXT)
+func checkDNSZoneRecordSets(t *testing.T, dnsZoneRecordSetsClient *armdns.RecordSetsClient, terraformOptions *terraform.Options) {
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "a_record_ids", "a_record_fqdns", armdns.RecordTypeA)
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "cname_record_ids", "cname_record_fqdns", armdns.RecordTypeCNAME)
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "ns_record_ids", "ns_record_fqdns", armdns.RecordTypeNS)
+	checkRecord(t, dnsZoneRecordSetsClient, terraformOptions, "txt_record_ids", "txt_record_fqdns", armdns.RecordTypeTXT)
 }
 
-func checkRecord(t *testing.T, dnsZoneRecordSetsClient dns.RecordSetsClient, terraformOptions *terraform.Options, recordSetIdsKey string, recordSetFQDNsKey string, recordType dns.RecordType) {
+func checkRecord(t *testing.T, dnsZoneRecordSetsClient *armdns.RecordSetsClient, terraformOptions *terraform.Options, recordSetIdsKey string, recordSetFQDNsKey string, recordType armdns.RecordType) {
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	zoneNames := terraform.OutputList(t, terraformOptions, "public_dns_zone_names")
 	recordSetIds := terraform.OutputMap(t, terraformOptions, recordSetIdsKey)
 	recordSetFQDNs := terraform.OutputMap(t, terraformOptions, recordSetFQDNsKey)
+	options := armdns.RecordSetsClientGetOptions{}
 
 	for _, zoneName := range zoneNames {
 		for key, recordSetId := range recordSetIds {
 			relativeRecordSetName := getSubdomain(recordSetFQDNs[key])
 
-			dnsZoneRecordSets, err := dnsZoneRecordSetsClient.Get(context.Background(), resourceGroupName, zoneName, relativeRecordSetName, recordType)
+			dnsZoneRecordSets, err := dnsZoneRecordSetsClient.Get(context.Background(), resourceGroupName, zoneName, relativeRecordSetName, recordType, &options)
 			if err != nil {
 				t.Fatalf("Error getting DNS Zone: %v", err)
 			}
@@ -63,7 +77,7 @@ func checkRecord(t *testing.T, dnsZoneRecordSetsClient dns.RecordSetsClient, ter
 			assert.Equal(t, strings.ToLower(recordSetId), strings.ToLower(*dnsZoneRecordSets.ID))
 			assert.Equal(t, strings.ToLower(relativeRecordSetName), strings.ToLower(*dnsZoneRecordSets.Name))
 			assert.Equal(t, strings.ToLower((string(recordType))), getRecordType(strings.ToLower(*dnsZoneRecordSets.Type)))
-			assert.NotEmpty(t, dnsZoneRecordSets.RecordSetProperties)
+			assert.NotEmpty(t, dnsZoneRecordSets.Properties)
 		}
 	}
 }
@@ -76,4 +90,16 @@ func getSubdomain(domain string) string {
 func getRecordType(input string) string {
 	parts := strings.Split(input, "/")
 	return parts[len(parts)-1]
+}
+
+func getCloudConfiguration(name string) (cloud.Configuration, error) {
+	switch strings.ToUpper(name) {
+	case "AZURECLOUD", "AZUREPUBLICCLOUD", "":
+		return cloud.AzurePublic, nil
+	case "AZUREUSGOVERNMENT", "AZUREUSGOVERNMENTCLOUD":
+		return cloud.AzureGovernment, nil
+	case "AZURECHINACLOUD":
+		return cloud.AzureChina, nil
+	}
+	return cloud.Configuration{}, fmt.Errorf("unknown cloud configuration name: %s", name)
 }
